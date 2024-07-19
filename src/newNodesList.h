dd4e59b2261d61bfb7a6f088e98d0fb806597c73
unicronTransform <jasonmagela@yahoo.com>
2024-07-19 12:27:06 -0400

    Minor changes

---
+++ src/newNodesList.h
@@ -0,0 +1,18 @@
+// new to repo
+
+
+
+
+// Some nodes I want to try adding to the repo
+
+
+
+
+// velocity node
+
+// acceleration node
+
+// skin cluster to neural network node
+
+// auto rivet node
+
